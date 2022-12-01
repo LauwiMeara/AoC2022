951c7690a7d3450451f5442f74630c89aff7c59c
Lauwi Meara <laurasusanne@live.nl>
2022-12-01 06:28:44 +0100

    Add Day01.kt

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,16 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<List<Int>>): Int {
+        return input.maxOf { it.sum() }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<List<Int>>): Int {
+        return input.map{it.sum()}.sortedDescending().subList(0, 3).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val input = readInputSplitByDelimiter("Day01","${System.lineSeparator()}${System.lineSeparator()}")
+        .map{it.split(System.lineSeparator())
+            .map{calories -> calories.toInt()}}
 
-    val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
 }
